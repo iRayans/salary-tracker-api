1a75c9a216bd79445f3e6d6b9facbc115c45d4cc
rharbi <iirharbi@gmail.com>
2025-04-05 12:53:24 +0300

    User model

---
+++ src/main/java/com/rayan/salarytracker/model/User.java
@@ -0,0 +1,113 @@
+package com.rayan.salarytracker.model;
+
+
+import com.rayan.salarytracker.core.enumuartion.UserRole;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "users")
+public class User extends PanacheEntity {
+
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    public String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    @Column(unique = true)
+    public String email;
+
+    @NotBlank(message = "Password is required")
+    public String password;
+
+    @Enumerated(EnumType.STRING)
+    public UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    public LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    public LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, UserRole role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
