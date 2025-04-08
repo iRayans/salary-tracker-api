d8d0c82e8492d9287d8968544dc6268092462af1
rharbi <iirharbi@gmail.com>
2025-04-08 18:58:53 +0300

    Implement BaseSalary related maps

--- src/main/java/com/rayan/salarytracker/mapper/Mapper.java
+++ src/main/java/com/rayan/salarytracker/mapper/Mapper.java
@@ -1,8 +1,11 @@
 package com.rayan.salarytracker.mapper;
 
 import com.rayan.salarytracker.core.enumuartion.UserRole;
+import com.rayan.salarytracker.dto.basesalary.BaseSalaryInsertDTO;
+import com.rayan.salarytracker.dto.basesalary.BaseSalaryReadOnlyDTO;
 import com.rayan.salarytracker.dto.user.UserInsertDTO;
 import com.rayan.salarytracker.dto.user.UserReadOnlyDTO;
+import com.rayan.salarytracker.model.BaseSalary;
 import com.rayan.salarytracker.model.User;
 import com.rayan.salarytracker.security.PasswordUtil;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -15,7 +18,8 @@ public class Mapper {
     // ======================================
 
     public User mapToUser(UserInsertDTO userInsertDTO) {
-        return new User(null,
+        return new User(
+                null,
                 userInsertDTO.getUsername(),
                 userInsertDTO.getEmail(),
                 PasswordUtil.encryptPassword(userInsertDTO.getPassword()),
@@ -26,7 +30,8 @@ public class Mapper {
     }
 
     public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
-        return new UserReadOnlyDTO(user.getId(),
+        return new UserReadOnlyDTO(
+                user.getId(),
                 user.getUsername(),
                 user.getEmail(),
                 user.getRole().name(),
@@ -35,4 +40,29 @@ public class Mapper {
         );
     }
 
+
+    // ============================================
+    // =          BaseSalary Mapper               =
+    // ============================================
+
+    public BaseSalary mapToBaseSalary(BaseSalaryInsertDTO baseSalaryInsertDTO) {
+        return new BaseSalary(
+                null,
+                baseSalaryInsertDTO.getAmount(),
+                baseSalaryInsertDTO.getDescription(),
+                null,
+                baseSalaryInsertDTO.isActive,
+                null,
+                null
+        );
+    }
+
+    public BaseSalaryReadOnlyDTO mapToBaseSalaryReadOnlyDTO(BaseSalary baseSalary) {
+        return new BaseSalaryReadOnlyDTO(
+                baseSalary.getId(),
+                baseSalary.getAmount(),
+                baseSalary.getDescription(),
+                baseSalary.isActive()
+        );
+    }
 }
